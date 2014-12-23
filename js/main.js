9b71452842588b78fe8be1f0caca9f39df51381d
James Power <jamespower_@hotmail.co.uk>
2014-12-23 16:28:58 +0000

    First complete basic design

--- js/main.js
+++ js/main.js
@@ -2,9 +2,12 @@ $(function() {
     // Init Controller
     var scrollMagicController = new ScrollMagic();
     // Create Animation for 0.5s
-    var tween = TweenMax.from('#mission', 1.5, {
-        left: 500,
-        ease: Power1.easeOut
+    var tween = TweenMax.from('.quote-right', 1, {
+        rotation: 360,
+        // transformOrigin: "right top",
+        xPercent: 300,
+        // scale: 10
+        // ease: Power1.easeOut
     });
     // Create the Scene and trigger when visible with ScrollMagic
     var scene = new ScrollScene({
